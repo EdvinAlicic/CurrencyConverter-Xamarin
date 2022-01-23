ae29f55a0d2303f56b57ea89c48a33f4f00e4788
edvin.alicic2020@outlook.com <edvin.alicic2020@outlook.com>
2022-01-23 22:41:31 +0100

    Add project files.

---
+++ RMAProjekat/RegisterPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RMAProjekat
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtRegisterPassword1.Text == txtRegisterPassword2.Text)
+            {
+                DisplayAlert("Uspjesna registracija", "Uspjesno ste se registrovali", "Ok");
+                Navigation.PopAsync();
+            }
+            else
+            {
+                DisplayAlert("Neispravan unos", "Sifre se ne podudaraju", "Ok");
+            }
+        }
+    }
+}
